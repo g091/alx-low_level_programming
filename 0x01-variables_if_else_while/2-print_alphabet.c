90a4ffab3c026ca3c5ca823e82f031b2ea5e8d58
g091 <gotieno791@gmail.com>
2022-03-12 02:19:26 +0300

    Lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * betty style doc for function goes there
+ * Return: Always return zero (0)
+ */
+
+int main(void)
+{
+	char small;
+
+	for (small = 'a'; small <= 'z'; small++)
+	putchar(small);
+	putchar('\n');
+	return (0);
+}

bb0550f48718e5fc82466a4e95833966fbe74a12
g091 <gotieno791@gmail.com>
2022-03-12 02:56:43 +0300

    Reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * betty style doc for function goes there
+ * Return: Always return zero (0)
+ */
+
+int main(void)
+{
+	char small;
+
+	for (small = 'z'; small >= 'a'; small++)
+	putchar(small);
+	putchar('\n');
+	return (0);
+}

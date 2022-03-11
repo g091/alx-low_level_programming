12eb2597de5b346ea77241f858c82ea586dc5db9
g091 <gotieno791@gmail.com>
2022-03-12 02:52:48 +0300

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * betty style doc for function goes there
+ * Return: Always return zero (0)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10 ; n++)
+	putchar(n + '0');
+	putchar('\n');
+	return (0);
+}

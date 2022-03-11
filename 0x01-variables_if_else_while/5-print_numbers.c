177c7790d63fec814f9cbaa529eef56834dd530e
g091 <gotieno791@gmail.com>
2022-03-12 02:46:11 +0300

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * betty style doc for function goes there
+ * Return: Always return zero (0)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10 ; n++)
+	printf("%i \n", n);
+	return (0);
+}

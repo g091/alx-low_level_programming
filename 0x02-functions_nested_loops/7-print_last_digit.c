99bc3b214b091d8994573db1ef8cbf7f02ff89cc
g091 <gotieno791@gmail.com>
2022-03-16 02:51:39 +0300

    last dig

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - last digit of no.
+ * @a: int
+ * Return: last digit
+ */
+
+int print_last_digit(int a)
+{
+	int b;
+
+	b = a % 10;
+	if (b < 0)
+	{
+		i *= -1;
+	}
+	_putchar(b + '0');
+	return (b);
+}
+

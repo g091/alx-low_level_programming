23d5c3bc4de62f786d8fb365d2b9c8461b6f1529
g091 <gotieno791@gmail.com>
2022-03-16 02:13:56 +0300

    Alpha char

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - checking for uppercase char
+ * @upp: char
+ * Return: 1 when lower, 0 when upper
+ */
+
+int _isalpha(int upp)
+{
+	if ((upp >= 'A' && upp <= 'Z') || (upp >= 'a' && upp <= 'z')
+		return (1);
+	else
+		return (0);
+}

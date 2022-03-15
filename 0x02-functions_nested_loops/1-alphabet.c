4491b27c1047e54daf85823f6a751aff61f9c2bf
g091 <gotieno791@gmail.com>
2022-03-16 01:36:47 +0300

    ALphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry point
+ * betty style doc for function goes there
+ * Return: Always return zero(0)
+ */
+
+void print_alphabet(void)
+{
+	char small;
+
+	for (small = 'a'; small <= 'z'; small++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+

bbbd175d6d5713553891b8a0c9e4412a4901d9a1
g091 <gotieno791@gmail.com>
2022-03-16 21:30:16 +0300

    n multiplication

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -38,7 +38,7 @@ void print_times_table(int n)
 				{
 					_putchar(' ');
 					_putchar((prod / 100) + '0');
-					_putchar((prod % 100) + '0');
+					_putchar(((prod % 100) / 10) + '0');
 					_putchar(((prod % 100) % 10) + '0');
 				}
 				if (j < n)

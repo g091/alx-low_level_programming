03d833f67c23376cd170bec19e7c999902483663
g091 <gotieno791@gmail.com>
2022-03-16 03:17:38 +0300

    Absolute v

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - checks no.
+ * @av: int
+ * Return:av when +ve
+ */
+
+int _abs(int av)
+{
+	if (av >= 0)
+	{
+		return (av);
+	}
+	else
+	{
+		return (-av);
+	}
+}

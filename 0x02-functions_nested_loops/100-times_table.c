2c6e7f0d78bdcff80fceb013ca3912460bf73b71
g091 <gotieno791@gmail.com>
2022-03-16 21:38:57 +0300

    n Mult

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,12 +17,11 @@ void print_times_table(int n)
 			_putchar('0');
 			if (n != 0)
 				_putchar(',');
-			for (j = 1; j <= n; j++)
+			for (j = 0; j <= n; j++)
 			{
 				prod = i * j;
 				if (prod < 10)
 				{
-					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar('0' + prod);
@@ -30,13 +29,11 @@ void print_times_table(int n)
 				else if (prod < 100)
 				{
 					_putchar(' ');
-					_putchar(' ');
 					_putchar((prod / 10) + '0');
 					_putchar((prod % 10) + '0');
 				}
 				else
 				{
-					_putchar(' ');
 					_putchar((prod / 100) + '0');
 					_putchar(((prod % 100) / 10) + '0');
 					_putchar(((prod % 100) % 10) + '0');

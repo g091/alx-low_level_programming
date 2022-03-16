f7835b3d0f25097e24d57f9525978503ae463aec
g091 <gotieno791@gmail.com>
2022-03-16 22:00:23 +0300

    100 t_t

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,37 +10,38 @@ void print_times_table(int n)
 {
 	int i, j, prod;
 
-	if (n >= 0 && n <= 15)
+	if (!(n > 15 || n < 0))
 	{
 		for (i = 0; i <= n; i++)
 		{
-			_putchar('0');
-			if (n != 0)
-				_putchar(',');
 			for (j = 0; j <= n; j++)
 			{
 				prod = i * j;
-				if (prod < 10)
+				if (j != 0)
 				{
+					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + prod);
 				}
-				else if (prod < 100)
+				if (prod < 10 && j != 0)
 				{
 					_putchar(' ');
+					_putchar(' ');
+					_putchar((prod % 10) + '0');
+				}
+				else if (prod >= 10 && prod < 100)
+				{
 					_putchar((prod / 10) + '0');
+					_putchar(((prod % 10) + '0');
+				}
+				else if (prod >= 100 && j != 0)
+				{	_putchar((prod / 100) + '0');
+					_putchar((prod / 10) % 10 + '0');
 					_putchar((prod % 10) + '0');
 				}
 				else
-				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod % 100) / 10) + '0');
-					_putchar(((prod % 100) % 10) + '0');
-				}
-				if (j < n)
-					_putchar(',');
+					_putchar((res % 10) + '0');
 			}
+			_putchar('\n');
 		}
 	}
 }

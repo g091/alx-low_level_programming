5936be950dfe34ee476dedfcc92e010b3ea28fb1
g091 <gotieno791@gmail.com>
2022-03-16 22:02:06 +0300

    100 t_t

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,7 +31,7 @@ void print_times_table(int n)
 				else if (prod >= 10 && prod < 100)
 				{
 					_putchar((prod / 10) + '0');
-					_putchar(((prod % 10) + '0');
+					_putchar((prod % 10) + '0');
 				}
 				else if (prod >= 100 && j != 0)
 				{	_putchar((prod / 100) + '0');

17280015c598f51d659f3cf4f393ab85d79e93b5
g091 <gotieno791@gmail.com>
2022-03-17 19:36:57 +0300

    digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isdigit - checking for digit from 0 to 10
+ * @n: int
+ * Return: 1 when digit else 0
+ */
+
+int _isdigit(int n)
+{
+	for (n = 0; n < 9; n++)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}

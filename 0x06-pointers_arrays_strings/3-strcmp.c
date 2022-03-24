50c21d429c8eee1e36ebfbc1c5a58f3e268eadca
g091 <gotieno791@gmail.com>
2022-03-24 21:54:18 +0300

    comparison of strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * int_strcmp - compares 2 strings
+ * @s1: 1st char
+ * @s2: 2nd char
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+			return(*s1 - *s2);
+
+		s1++;
+		s2++;
+
+	}
+	return(0);
+}

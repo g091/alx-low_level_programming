1090944d892baeabccd6eccf357f170865df7e40
g091 <gotieno791@gmail.com>
2022-03-24 23:45:05 +0300

    to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes to uppercase
+ * @str: string
+ * Return: uppercase string
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 'a' + 'A';
+	}
+	return (str);
+}

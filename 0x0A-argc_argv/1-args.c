e1cfe1e3cf55a40137b7ffef47f6d7e8477b44e6
g091 <gotieno791@gmail.com>
2022-04-03 00:39:52 +0300

    no. of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the no. of arguenments passed to it
+ * @argc: arguemnet count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}

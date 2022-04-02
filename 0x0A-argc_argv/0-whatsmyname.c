16da4685264488f2c7cef24c0180c41de5182d02
g091 <gotieno791@gmail.com>
2022-04-03 00:33:24 +0300

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -  writes it's name
+ * @argc: arguement count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", *argv);
+	return (0);
+}

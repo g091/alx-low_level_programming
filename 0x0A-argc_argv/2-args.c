191891aef55d29f6054d48f809eb6005394e701c
g091 <gotieno791@gmail.com>
2022-04-03 00:43:58 +0300

    print args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prog. that prints all arguments it receives
+ * @argc: arguement count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}

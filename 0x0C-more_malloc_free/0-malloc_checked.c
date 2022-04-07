41fb1ca7a2b248b5a435208579e29945c7f0fdbe
g091 <gotieno791@gmail.com>
2022-04-08 02:02:29 +0300

    malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,11 +10,10 @@
 
 void *malloc_checked(unsigned int b)
 {
-	int *ptr;
+	char *ptr;
 
-	ptr = malloc(sizeof(b));
+	ptr = malloc(b);
 	if (ptr == NULL)
 		exit(98);
 	return (ptr);
-	free(ptr);
 }

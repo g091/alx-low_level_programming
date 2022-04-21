8e892c25c10d8de4fe2a503cb873e4a0a08f18d0
g091 <gotieno791@gmail.com>
2022-04-22 02:19:33 +0300

    print elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list
+ * @h: singly linked list.
+ * Return: elements in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t list;
+
+	list = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		list++;
+	}
+	return (list);
+}

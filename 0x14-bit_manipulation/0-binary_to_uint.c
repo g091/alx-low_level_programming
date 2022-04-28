582c34f308422185437d26a2086b71609a7ab3f1
g091 <gotieno791@gmail.com>
2022-04-29 02:41:26 +0300

    bin to u_int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 	if (b[i] == '\0')
 		return (0);
 
-	while (b[i] == '0' || b[i] == '1')
+	while ((b[i] == '0') || (b[i] == '1'))
 	{
 		val <<= 1;
 		val += b[i] - '0';

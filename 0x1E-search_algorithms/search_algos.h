c74f8fbe215330d5d43008c536d2389743e2a44e
g091 <gotieno791@gmail.com>
2022-10-06 00:34:30 +0300

    main

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef _SEARCH_ALGOS_
+#define _SEARCH_ALGOS_
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
